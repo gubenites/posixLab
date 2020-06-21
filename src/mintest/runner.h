42ede6a92e4c863b54ff3e9ce3868b68ee614011
gubenites <gubenites99@gmail.com>
2020-06-21 05:38:13 -0300

    atualizado

--- src/mintest/runner.h
+++ src/mintest/runner.h
@@ -25,8 +25,9 @@ int main(int argc, char *argv[]) {
             if (WIFEXITED(wt)) {
                 printf("Filhou acabou: %d\n",(char) WEXITSTATUS(wt));
                 pass_count++;
-                printf("\n\n=====================\n");
+
                 printf("%d/%d tests passed\n", pass_count, size);
+                printf("\n\n=====================\n");
             }
           }
         }
