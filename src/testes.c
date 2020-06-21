f616277c716d5b21f0f5430bc79cb47eec072b7e
gubenites <gubenites99@gmail.com>
2020-06-21 01:53:00 -0300

    atualizado

---
+++ src/testes.c
@@ -0,0 +1,51 @@
+#include "mintest/macros.h"
+
+int test1(){
+  test_assert(1/0, "Divisão por zero, não dara certo\n");
+  return 0;
+}
+
+int test2(){
+  int counter = 0;
+
+  while(){
+    counter += 1;
+  }
+
+  printf("ERRO, LOOP INFINITO\n");
+  return 0;
+}
+
+int test3(){
+  int counter = 0;
+  for(int i = 0; i < 200000 ; i++){
+    counter += 1;
+  }
+  return 0;
+}
+
+int test4(){
+  test_assert(4 == 4, "Esse teste ira passar\n");
+  test_assert(1 != 1 , "Esse teste não ira passar\n");
+
+  return 0;
+}
+
+int test5(){
+  for(int i = 0 ; i < 50 ; i++){
+    printf("Numero de prints %d,IGOR", i);
+  }
+
+  return 0;
+}
+
+int test6(){
+  test_assert(1 == 1, "Teste rapido");
+  test_assert(2 != 6, "Teste lento");
+
+  return 0;
+}
+
+test_list = { TEST(test1), TEST(test2), TEST(test3), TEST(test4) , TEST(test5) , TEST(test6) };
+
+#include "mintest/runner.h"
