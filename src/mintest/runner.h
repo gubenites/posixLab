e68eb06a017529aef565f133082db8836d01e32d
gubenites <gubenites99@gmail.com>
2020-06-21 02:06:47 -0300

    atualizado

--- src/mintest/runner.h
+++ src/mintest/runner.h
@@ -11,6 +11,7 @@ int main(int argc, char *argv[]) {
     int pass_count = 0;
     for (int i = 0; i < size; i++) {
       printf("LOOP: %d\n", i);
+      printf("TEST: %d\n", all_tests[i].function());
         if (all_tests[i].function() >= 0) {
             printf("TEST: %d\n", i + 1);
             printf("%s: [PASS]\n", all_tests[i].name);
